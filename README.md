fee58d8fa8911978d6276260d3d2c82a2ea7b015
Acute Interpreter Panic <223899499+acute-interpreter-panic@users.noreply.github.com>
2025-07-10 12:41:09 +0200

    added store filter

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # trans new tab page
 
 https://diyhrt.market/api/
+
+## development
+
+1. Install [air](https://github.com/air-verse/air)
+2. Run air
+
+```sh
+air
+```
---
+++ internal/diyhrt/filter.go
@@ -0,0 +1,38 @@
+package diyhrt
+
+import "slices"
+
+type StoreFilter struct{
+    Limit int
+
+    IncludeIds []int
+    ExcludeIds []int
+}
+
+
+func (f StoreFilter) Filter (stores []Store) []Store {
+    result := make([]Store, 0)
+
+    for _, s := range stores {
+        if f.Limit > 0 && len(result) >= f.Limit {
+            break
+        }
+
+        if len(f.IncludeIds) > 0 {
+            if slices.Contains(f.IncludeIds, s.Id) {
+                result = append(result, s)
+            }
+            continue
+        }
+
+
+        if slices.Contains(f.ExcludeIds, s.Id) {
+            continue
+        }
+
+
+        result = append(result, s)
+    }
+
+    return  result
+}
