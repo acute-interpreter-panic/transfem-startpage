a602db3e93386f8fd57bd6838a219bae5496c518
Acute Interpreter Panic <223899499+acute-interpreter-panic@users.noreply.github.com>
2025-07-11 12:26:20 +0200

    opening urls directly

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -1,1 +1,38 @@
-console.log("included search")
+console.log("adding features to search...");
+
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-input");
+
+const searchEngines = {
+    "g": {
+        action: "https://www.google.com/search/",
+        name: "q",
+    },
+    "d": {
+        action: "https://duckduckgo.com/",
+        name: "q",
+    },
+    "y": {
+        action: "https://yandex.com/search/",
+        name: "text",
+    },
+    "lure": {
+        action: "https://lure.sh/pkgs",
+        name: "q",
+    },
+};
+
+// https://stackoverflow.com/a/3809435/16804841
+const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+const urlRegex = new RegExp(expression);
+
+form.addEventListener("submit", event => {
+    s = input.value;
+
+    // check if url
+    if (s.match(urlRegex)) {
+        event.preventDefault();
+        window.open(s, "_self");
+        return
+    }
+});
