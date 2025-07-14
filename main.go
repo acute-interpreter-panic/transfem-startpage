7576011ea07b253a2c7d5db581eb10b7b320b9b4
Acute Interpreter Panic <223899499+acute-interpreter-panic@users.noreply.github.com>
2025-07-14 14:30:59 +0200

    using embeded file system for static files

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 	"os"
 	"strconv"
@@ -12,9 +14,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-
 var CurrentConfig = rendering.NewConfig()
-
 
 func FetchDiyHrt() error {
 	fmt.Println("Fetch DiyHrt Marketplaces...")
@@ -32,6 +32,18 @@ func getIndex(c echo.Context) error {
 	rendering.IndexTemplate.Execute(&tpl, CurrentConfig.Template)
 
 	return c.HTML(http.StatusOK, tpl.String())
+}
+
+//go:embed frontend/*
+var frontendFiles embed.FS
+
+func getFileSystem() http.FileSystem {
+	fsys, err := fs.Sub(frontendFiles, "frontend")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
 }
 
 func main() {
@@ -54,8 +66,11 @@ func main() {
 	}
 
 	e := echo.New()
-	e.Static("/assets", "frontend/assets")
-	e.Static("/scripts", "frontend/scripts")
+	// "frontend/assets"
+
+	staticHandler := http.FileServer(getFileSystem())
+	e.GET("/assets/*", echo.WrapHandler(http.StripPrefix("/", staticHandler)))
+	e.GET("/scripts/*", echo.WrapHandler(http.StripPrefix("/", staticHandler)))
 	e.GET("/", getIndex)
 
 	e.Logger.Fatal(e.Start(":" + strconv.Itoa(CurrentConfig.Server.Port)))
